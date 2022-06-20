ae298df093f6b38981a5db9b20346596ddd5d7f1
AnnaLvova <annlvova.7th@gmail.com>
2022-06-20 22:24:18 +0700

    string with slash quotes

--- js/main.js
+++ js/main.js
@@ -17,15 +17,15 @@ function getRandomPositiveFloat (a, b, digits = 1) {
 
 // случайный элемент массива
 function getRandomArrayElement (elements) {
-    return elements[getRandomInteger(0, elements.length - 1)];
+    return elements[getRandomPositiveInteger(0, elements.length - 1)];
 }
 
 // случайный перемешанный массив случайно длины из заданных элементов
 function getShuffledUniqueElements (elements) {
-    const elementsCount = getRandomInteger(1, elements.length);
+    const elementsCount = getRandomPositiveInteger(1, elements.length);
     const shuffledElements = [];
     while (shuffledElements.length < elementsCount) {
-        const randomIndex = getRandomInteger(0, elementsCount - 1);
+        const randomIndex = getRandomPositiveInteger(0, elementsCount - 1);
         if (!shuffledElements.includes(elements[randomIndex])) {
             shuffledElements.push(elements[randomIndex]);
         }
@@ -33,18 +33,18 @@ function getShuffledUniqueElements (elements) {
     return shuffledElements;
 }
 
-let avatarNumber =  getRandomPositiveInteger(1, 10);
+const avatarNumber =  getRandomPositiveInteger(1, 10);
 
-let author = {
-    avatar: 'img/avatars/user`${(avatarNumber.padStart(2, '0')}`.png'
+const author = {
+    avatar: `img/avatars/user${avatarNumber.padStart(2, '0')}.png`
 };
 // где {{xx}} — это число от 1 до 10. Перед однозначными числами ставится 0. Например, 01, 02...10. Адреса изображений не повторяются.
 
-let offerTitle = ['Санаторий', 'Отель', 'Глэмпинг', 'Гостевой Дом', 'Турбаза', 'Хостел', 'Пансионат'];
-let housingType = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-let checkinTime = ['12:00', '13:00', '14:00'];
-let offerFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner' ];
-let offerDescriptions = [
+const offerTitle = ['Санаторий', 'Отель', 'Глэмпинг', 'Гостевой Дом', 'Турбаза', 'Хостел', 'Пансионат'];
+const housingType = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const checkinTime = ['12:00', '13:00', '14:00'];
+const offerFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner' ];
+const offerDescriptions = [
     'Трехразовое питание',
     'Уютное место',
     'Дискотека каждый вечер',
@@ -53,18 +53,18 @@ let offerDescriptions = [
     'Бассейн с морской водой'
 ];
 
-let offerPhotos = [
+const offerPhotos = [
     'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
     'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
     'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
 ];
 
-let location = {
+const location = {
     lat: getRandomPositiveFloat(35.65000, 35.70000, 5),
     lng: getRandomPositiveFloat(139.70000, 139.80000, 5),
 };
 
-let offer = {
+const offer = {
    title: getRandomArrayElement(offerTitle),
    address: `${location.lat}, ${location.lng}`,
    price: getRandomPositiveInteger(minPrice, maxPrice),
@@ -78,13 +78,11 @@ let offer = {
 };
 
 
-let createAdvertisement = () => {
-    return {
-        autor: author,
-        offer: offer,
-        location
-    };
-  }
+const createAdvertisement = () => ({
+    autor: author,
+    offer: offer,
+    location
+});
 
-  const similarAdvertisements = Array.from({length: 10}, createAdvertisement);
-  console.log(similarAdvertisements);
+const similarAdvertisements = Array.from({length: 10}, createAdvertisement);
+console.log(similarAdvertisements);
